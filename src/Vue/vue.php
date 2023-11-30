67f9c8a7cad1df6d633ee70c37cf88476195e616
Dudiot Dany <dany.dudiot@gmail.com>
2023-11-30 21:17:20 +0100

    Dev : new flie Exception.php and documentation

--- src/Vue/vue.php
+++ src/Vue/vue.php
@@ -1,18 +1,69 @@
 <?php
+/**
+ * Fonction qui affiche la page d'accueil du directeur
+ * Ne prend pas de paramètres et ne retourne rien
+ */
+function afficherAccueilDirecteur(){
+    $contenu="";
+    require_once('gabaritDirecteurHomePage.php');
+}
 
+/**
+ * Fonction qui affiche la page d'accueil du conseiller
+ * Ne prend pas de paramètres et ne retourne rien
+ */
+function afficherAccueilConseiller(){
+    $contenu="";
+    require_once('gabaritConseillerHomePage.php');
+}
+/**
+ * Fonction qui affiche la page d'accueil de l'agent d'accueil
+ * Ne prend pas de paramètres et ne retourne rien
+ */
+function afficherAccueilAgent(){
+    $contenu="";
+    require_once('gabaritAccueilHomePage.php');
+}
+/**
+ * Fonction qui affiche la page de login
+ * Ne prend pas de paramètres et ne retourne rien
+ */
 function afficherLogin(){
     $contenu="";
     require_once('gabaritLanding.php');
 }
-function afficherErreur ($erreur) {
-    $content = "<p>".$erreur."</p><p><a href=\"index.php\"/> Revenir au forum </a></p>";
-    require_once('gabaritLanding.php');
-}
+
+
+/**
+ * Fonction qui affiche la page du client
+ * Ne retourne rien
+ * @param string $client c'est les données du client
+ */
 function afficherClient($client){
     $contenu= '';
     require_once('gabaritInfoClient.php');
 }
-
+/**
+ * Fonction qui affiche la page de resultat de recherche d'un client
+ * Ne retourne rien
+ * @param array $listeClient c'est la liste des clients
+ */
 function afficherRechercheClient($listeClient) {
-    $contenu="";
+    $contenu="<table>";
+    foreach ($listeClient as $client) {
+        $contenu .= "<tr><td>".$client['id']."</td><td>".$client['nom']."</td><td>".$client['prenom']."</td><td>".$client['dateNaissance']."</td>";
+    }
+    $contenu .= "</table>";
+    require_once('gabaritRechercheClient.php');
+}
+
+
+/**
+ * Fonction qui affiche la page d'erreur
+ * Ne retourne rien
+ * @param string $erreur
+ */
+function afficherErreur ($erreur) {
+    $content = "<p>".$erreur."</p><p><a href=\"index.php\"/> Revenir au forum </a></p>";
+    require_once('gabaritLanding.php');
 }
