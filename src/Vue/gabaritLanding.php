8ab50aafd36df19bcd5846d2484e6ded1541e1f0
Kaawan-d20 <kaawan.jdr@gmail.com>
2023-11-30 12:47:51 +0100

    added Style.css

--- src/Vue/gabaritLanding.php
+++ src/Vue/gabaritLanding.php
@@ -3,14 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Landing</title>
 </head>
 <body>
-                <!-- TODO -->
-    <form action="index.php" method="POST">
-        <label for="loginField">Nom d'utilisateur</label><input type="text" name="loginField" id="loginField">
-        <label for="passwordField">Mot de Passe</label><input type="password" name="passwordField" id="passwordField">
-        <input type="submit" name="" id="" value="Submit">
-    </form>
+    <div class="landingWrapper">
+        <form action="index.php" method="POST">
+        <fieldset>
+            <legend>Connection</legend>
+            <p>
+                <label for="landingLoginField">Nom d'utilisateur</label>
+                <input type="text" name="landingLoginField" id="landingLoginField">
+            </p>
+            <p>
+                <label for="landingPasswordField">Mot de Passe</label>
+                <input type="password" name="landingPasswordField" id="landingPasswordField">
+            </p>
+            <p>
+                <input type="submit" name="landingSubmitBtn"  id="landingSubmitBtn" value="Connection">
+                <input type="reset" name="landingResetBtn" id="landingResetBtn">
+            </p>
+        </fieldset>
+        </form>
+    </div>
 </body>
 </html>
