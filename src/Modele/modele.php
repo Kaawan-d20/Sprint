1a8c37867bef12fb336b183a8911fbb2fcfdc453
Tenebiscotta <night0wl42027@gmail.com>
2023-11-30 12:47:31 +0100

    On va graille

--- src/Modele/modele.php
+++ src/Modele/modele.php
@@ -2,7 +2,36 @@
 require_once 'modele/token.php';
 require_once 'token.php';
 
-
-function afficherErreur ($erreur) {
-    $content = "<p>".$error."</p><p><a href=\"index.php\"/> Revenir au forum </a></p>";
+function modGetSalt($login) {
+    $connection = Connection::getInstance()->getConnection();
+    $query = $connection -> prepare("SELECT salt FROM employe WHERE login=:logi");
+    $prepared = $connection->prepare($query);
+    $prepared -> bindParam(":logi", $login, PDO::PARAM_STR);
+    $prepared -> execute();
+    $prepared -> setFetchMode(PDO::FETCH_OBJ);
+    $prepared -> closeCursor();
+    return $prepared;
 }
+
+function modConnect($login, $password) {
+    $connection = Connection::getInstance()->getConnection();
+    $query = 'SELECT idemploye FROM employe WHERE login=:logi AND password=:pass';
+    $prepared = $connection->prepare($query);
+    $prepared -> bindParam(':logi',$login,PDO::PARAM_STR);
+    $prepared -> bindParam(':pass',$password,PDO::PARAM_STR);
+    $prepared -> execute();
+    $prepared -> setFetchMode(PDO::FETCH_OBJ);
+    $prepared -> closeCursor();
+    return $prepared;
+}
+
+function modGetClientFromId($idClient) {
+    $connection = Connection::getInstance()->getConnection();
+    $query = 'SELECT * FROM client WHERE idClient=:idC';
+    $prepared = $connection -> prepare($query);
+    $prepared -> bindParam(':idC', $idClient, PDO::PARAM_STR);
+    $prepared -> execute();
+    $prepared -> setFetchMode(PDO::FETCH_OBJ);
+    $prepared -> closeCursor();
+}
+
