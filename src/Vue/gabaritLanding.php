60ec73e5341bfa5f122522a7ae3ab544d1a13090
Kaawan-d20 <kaawan.jdr@gmail.com>
2023-12-01 12:24:23 +0100

    updated the look of the Landing page

--- src/Vue/gabaritLanding.php
+++ src/Vue/gabaritLanding.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/31ad525f9a.js" crossorigin="anonymous"></script>
     <title>Landing</title>
 </head>
 <body>
@@ -16,14 +17,15 @@
             <form action="index.php" method="POST" class="loginForm">
                 <div class="loginFormFieldWrapper">
                     <label for="landingLoginField" class="visually-hidden">Nom d'utilisateur</label>
-                    <input type="text" name="landingLoginField" id="landingLoginField" class="loginFormField">
+                    <input type="text" name="landingLoginField" id="landingLoginField" class="loginFormField" placeholder="Login">
                 </div>
                 <div class="loginFormFieldWrapper">
                     <label for="landingPasswordField" class="visually-hidden">Mot de Passe</label>
-                    <input type="password" name="landingPasswordField" id="landingPasswordField" class="loginFormField">
-                    <input type="checkbox" name="showPassword" id="showPassword" onclick="togglePasswordVisibility()">
+                    <input type="password" name="landingPasswordField" id="landingPasswordField" class="loginFormField" placeholder="Password">
+                    <button onclick="togglePasswordVisibility()" type="button" class="visibilityButton"><i class="fa-solid fa-eye-slash" id="visibilityIcon"></i></button>
                 </div>
-                <div>
+                <p>Pas de compte ? <a href="i dont know">en créer un</a></p>
+                <div class="ctaContainer">
                     <input type="submit" name="landingSubmitBtn"  id="landingSubmitBtn" value="Connection" class="cta">
                 </div>
             </form>
@@ -32,11 +34,16 @@
 </div>
 <script>
     function togglePasswordVisibility() {
-        var passwordField = document.getElementById("landingPasswordField");
+        let passwordField = document.getElementById("landingPasswordField");
+        let icon = document.getElementById("visibilityIcon");
         if (passwordField.type === "password") {
             passwordField.type = "text";
+            icon.classList.remove("fa-eye");
+            icon.classList.add("fa-eye-slash");
         } else {
             passwordField.type = "password";
+            icon.classList.remove("fa-eye-slash");
+            icon.classList.add("fa-eye");
         }
 }
 </script>
