d427125c8f039f6443ce1782e2839391d42806c3
Kaawan-d20 <kaawan.jdr@gmail.com>
2023-12-02 11:24:59 +0100

    fixed my sh*it, renamed the wrong file

--- src/Vue/gabaritAgentHomePage.php
+++ src/Vue/gabaritAgentHomePage.php
@@ -3,10 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Accueil</title>
 </head>
 <body>
-    <h1>Calendar</h1>
-    <h1>Recherche Client</h1>
+    <h1>Accueil Agent</h1>
+    <form action="Change this">
+        <label for="searchClientField">Chercher un client</label>
+        <input type="number" name="searchClientField" id="searcClientField">
+        <label for="idClient">Debit Credit</label><input type="number" name="idClient" id="idClient">
+        <label for="calendar">calendar</label><input type="date" name="calendar" id="calendar">
+    </form>
 </body>
 </html>
